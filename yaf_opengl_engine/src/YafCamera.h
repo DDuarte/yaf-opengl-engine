8277c67a81f900e3b7111584468c1a68a7b5781a
Nay <dnpd.dd@gmail.com>
2013-10-09 12:09:02 +0100

    Fix cameras

--- yaf_opengl_engine/src/YafCamera.h
+++ yaf_opengl_engine/src/YafCamera.h
@@ -16,7 +16,7 @@ public:
 class YafPerspectiveCamera : public YafCamera
 {
 public:
-    YafPerspectiveCamera(const std::string& id, YafXYZ pos) : YafCamera(id), _firstTime(true)
+    YafPerspectiveCamera(const std::string& id, YafXYZ pos) : YafCamera(id)
     {
         position[0] = pos.X;
         position[1] = pos.Y;
@@ -30,21 +30,10 @@ public:
     {
         glMatrixMode(GL_MODELVIEW);
 
-        if (_firstTime)
-        {
-            glLoadIdentity();
-            gluLookAt(position[0], position[1], position[2],
-                Target.X, Target.Y, Target.Z,
-                0.0, 1.0, 0.0);
-            _firstTime = false;
-        }
-        else
-        {
-            glTranslatef(position[0], position[1], position[2]);
-            glRotatef(rotation[0], 1.f, 0.f, 0.f);
-            glRotatef(rotation[1], 0.f, 1.f, 0.f);
-            glRotatef(rotation[2], 0.f, 0.f, 1.f);
-        }
+        glLoadIdentity();
+        gluLookAt(position[0], position[1], position[2],
+            Target.X, Target.Y, Target.Z,
+            0.0, 1.0, 0.0);
     }
 
     virtual void updateProjectionMatrix(int width, int height) override
@@ -53,9 +42,6 @@ public:
         glLoadIdentity();
         gluPerspective(Angle, (double) width / (double) height, Near, Far);
     }
-
-private:
-    bool _firstTime;
 };
 
 class YafOrthoCamera : public YafCamera
