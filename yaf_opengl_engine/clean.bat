e1bae5a198ecf33425379c646b459f7713f808f4
Nay <dnpd.dd@gmail.com>
2013-11-20 10:38:11 +0000

    Delete /docs from repository

---
+++ yaf_opengl_engine/clean.bat
@@ -0,0 +1,18 @@
+del yaf_opengl_engine.opensdf
+del yaf_opengl_engine.sdf
+del /F yaf_opengl_engine.v11.suo
+del /F yaf_opengl_engine.v12.suo
+del bin\yaf.bsc
+del bin\yaf.ilk
+del bin\yaf.pdb
+rmdir temp /Q /S
+rmdir dep\CGFlib\bin /Q /S
+rmdir dep\CGFlib\temp /Q /S
+rmdir dep\freeglut\VisualStudio\2012\bin /Q /S
+rmdir dep\freeglut\VisualStudio\2012\temp /Q /S
+rmdir dep\glew\build\vc10\bin /Q /S
+rmdir dep\glew\build\vc10\temp /Q /S
+rmdir dep\glui\bin /Q /S
+rmdir dep\glui\temp /Q /S
+rmdir dep\tinyxml\bin /Q /S
+rmdir dep\tinyxml\temp /Q /S
