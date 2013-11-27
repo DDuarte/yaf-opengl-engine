4d365d31ee8287609774ad1bfe29d1a48d6b76ce
DDuarte <dnpd.dd@gmail.com>
2013-11-27 19:23:07 +0000

    Position camera to point at board

--- yaf_opengl_engine/src/YafCamera.h
+++ yaf_opengl_engine/src/YafCamera.h
@@ -95,10 +95,13 @@ public:
         else
         {
             glLoadIdentity();
-            glTranslatef(position[0], position[1], position[2]);
-            glRotatef(rotation[0], 1.f, 0.f, 0.f);
-            glRotatef(rotation[1], 0.f, 1.f, 0.f);
-            glRotatef(rotation[2], 0.f, 0.f, 1.f);
+            //glTranslatef(position[0], position[1], position[2]);
+            //glRotatef(rotation[0], 1.f, 0.f, 0.f);
+            //glRotatef(rotation[1], 0.f, 1.f, 0.f);
+            //glRotatef(rotation[2], 0.f, 0.f, 1.f);
+            gluLookAt(position[0], position[1], position[2],
+            Target.X, Target.Y, Target.Z,
+                0.0, 1.0, 0.0);
         }
     }
 
